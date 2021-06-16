e3953d865fe8ba807b13d1db8f890621565c3788
itotto0205 <itotto0205@gmail.com>
2021-06-17 00:28:54 +0900

    単語のカウント追加

---
+++ wordcount001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wordcount001 {
+    /// <summary>
+    /// 単語のカウント
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/skillcheck_archive/word-count/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            // TODO:速度に問題あり
+
+            var words = Console.ReadLine().Split(' ');
+            var r = new Dictionary<string, int>();
+            var order = new List<string>();
+
+            foreach (var w in words) {
+                if (!order.Contains(w)) order.Add(w);
+                r[w] = r.ContainsKey(w) ? r[w] + 1 : 1;
+            }
+            order.ForEach(o => Console.WriteLine($"{o} {r[o]}"));
+        }
+    }
+}
