5b177a6c0b96ac64164ef8d4cff477ea7cfe91de
itotto0205 <itotto0205@gmail.com>
2021-06-19 02:22:38 +0900

    野球の審判 追加

---
+++ baseballumpire001/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace baseballumpire001 {
+    /// <summary>
+    /// 野球の審判
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/skillcheck_archive/umpire/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var strike = 0;
+            var ball = 0;
+            var result = new StringBuilder();
+            for (var i = 0; i < n; i++) {
+                var r = Console.ReadLine();
+                if (r == "strike") strike++;
+                else if (r == "ball") ball++;
+
+                if (strike == 3 || ball == 4) {
+                    result.AppendLine($"{(r == "strike" ? "out!" : "fourball!")}");
+
+                    strike = 0;
+                    ball = 0;
+                } else {
+                    result.AppendLine($"{r}!");
+                }
+            }
+
+            // 結果の表示
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
