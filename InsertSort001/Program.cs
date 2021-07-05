8e8730592b544aae653774d24c52d03137ffbfd1
itotto0205 <itotto0205@gmail.com>
2021-07-06 00:31:38 +0900

    自己紹介追加 挿入ソート追加（未了）

---
+++ InsertSort001/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertSort001 {
+    /// <summary>
+    /// 挿入ソート
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/sort_naive/sort_naive__insertion?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            Action<int[]> previewArray = (ar) => {
+                for (var x = 0; x < ar.Length; x++) {
+                    Console.Write($"{(x != 0 ? " " : string.Empty)}{ar[x]}");
+                }
+                Console.WriteLine();
+            };
+
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            // データを取り込む
+            var sortedArray = new int[n];
+            var data = Console.ReadLine().Split(' ');
+            for (var i = 0; i < n; i++) {
+                sortedArray[i] = Convert.ToInt32(data[i]);
+            }
+
+            // 並び替え
+            for (var i = 1; i < n; i++) {
+                var targetValue = sortedArray[i];
+                var pos = -1;
+                for (var j = 0; j < n; j++) {
+                    if (targetValue < sortedArray[j]) {
+                        pos = j;
+                        break;
+                    }
+                }
+
+                // 一番大きかったら一番後ろに並べる
+                if (pos == -1) pos = n - 1;
+
+                // 並べ替える必要がなければそのまま
+                if (pos != i) {
+                    for (var j = i; i < n; j++) {
+
+                    }
+                }
+
+                // 結果を表示
+                previewArray(sortedArray);
+            }
+
+        }
+    }
+}
---
+++ selfintroduction001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace selfintroduction001 {
+    /// <summary>
+    /// 自己紹介
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/self_introduction/self_introduction__1/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var name = "itotto";
+            var language = "C#";
+            var hitokoto = "let's enjoy!";
+
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"language: {language}");
+            Console.WriteLine($"hitokoto: {hitokoto}");
+        }
+    }
+}
