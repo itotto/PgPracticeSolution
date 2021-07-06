da0013843b13579dcfb35ba663846e13e977b7b7
itotto0205 <itotto0205@gmail.com>
2021-07-07 01:24:54 +0900

    追加忘れ

--- numbersort001/Program.cs
+++ numbersort001/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace numbersort001 {
+    /// <summary>
+    /// 数の並び替え
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/etc/sort-number/edit?language_uid=c-sharp</remarks>
     class Program {
-        static void Main(string[] args) {
+        static void Main() {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var numbers = new List<int>(n);
+
+            for (var i = 0; i < n; i++) {
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            numbers.Sort((x, y) => x - y);
+            numbers.ForEach(num => Console.WriteLine(num));
         }
     }
 }
