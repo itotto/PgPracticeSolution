0228beafdd822d97478a6a23db13e13e6286e2b8
itotto0205 <itotto0205@gmail.com>
2021-07-07 22:50:24 +0900

    ifを使いますか？三項演算子を使いますか？ 追加

---
+++ iforternaryoperator001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace iforternaryoperator001 {
+    /// <summary>
+    /// ifを使いますか？三項演算子を使いますか？
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/etc/ternary/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a > 0 ? "plus" : a.ToString());
+
+            var b = Console.ReadLine();
+            Console.WriteLine(b == "hoge" ? "yes" : b);
+
+            var c = Console.ReadLine();
+            Console.WriteLine(c.Length == 10 ? "ten" : c);
+
+            var d = Console.ReadLine();
+            var dpos = d.IndexOf('x');
+            Console.WriteLine(dpos >= 0 ? dpos.ToString() : "nothing");
+
+            var e = Console.ReadLine();
+            Console.WriteLine(e.Length ==  5 ? "five" : e[0].ToString());
+        }
+    }
+}
