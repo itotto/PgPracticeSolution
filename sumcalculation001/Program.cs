dd9c2219b332a13a37bb441ec627d2e9e551b803
itotto0205 <itotto0205@gmail.com>
2021-07-09 23:54:03 +0900

    総和の計算追加

---
+++ sumcalculation001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sumcalculation001 {
+    /// <summary>
+    /// 総和の計算
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/etc/a_rank_calculation_step1?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var conditions = Console.ReadLine().Split(' ');
+            var a = Convert.ToDecimal(conditions[0]);
+            var b = Convert.ToDecimal(conditions[1]);
+
+            Console.WriteLine((a + b) * (b - a + 1) / 2);
+        }
+    }
+}
