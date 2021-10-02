162395c6ecd8f880f57e9b0e2d27907b94bdd52f
itotto0205 <itotto0205@gmail.com>
2021-10-03 00:34:31 +0900

    2番目に大きな値 追加 k番目に大きな値 追加

---
+++ kthargestvalue001/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kthargestvalue001 {
+    /// <summary>
+    /// k番目に大きな値
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/sequence_search_problems/sequence_search_problems_kthmax_boss/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            _ = Convert.ToInt32(Console.ReadLine());
+
+            var values = new List<int>();
+            var inputs = Console.ReadLine().Split(' ');
+            for (var i = 0; i < inputs.Length; i++) {
+                values.Add(Convert.ToInt32(inputs[i]));
+            }
+
+            var k = Convert.ToInt32(Console.ReadLine());
+
+            // 並び替え
+            values.Sort((x, y) => y - x);
+
+
+            // 結果の表示
+            Console.WriteLine(values[k - 1]);
+        }
+    }
+}
---
+++ secondlargestvalue001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace secondlargestvalue001 {
+    /// <summary>
+    /// 2番目に大きな値
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/sequence_search_problems/sequence_search_problems_kthmax_step0/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            _ = Convert.ToInt32(Console.ReadLine());
+
+            var values = new List<int>();
+            var inputs = Console.ReadLine().Split(' ');
+            for (var i = 0; i < inputs.Length; i++) {
+                values.Add(Convert.ToInt32(inputs[i]));
+            }
+
+            // 並び替え
+            values.Sort((x, y) => y - x);
+
+
+            // 結果の表示
+            Console.WriteLine(values[1]);
+        }
+    }
+}
