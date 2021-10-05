bbd73daafadf47e5b6f27dcc27a697c4023a2351
itotto0205 <itotto0205@gmail.com>
2021-10-06 00:50:06 +0900

    エラトステネスの篩 追加

---
+++ eratosthenessieve001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace eratosthenessieve001 {
+    /// <summary>
+    /// エラトステネスの篩
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/prime_number_primer/prime_number_primer__eratosthenes/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var isPrime = new bool[n + 1];
+            isPrime[0] = false;
+            isPrime[1] = false;
+            for (var i = 2; i <= n; i++) isPrime[i] = true;
+
+            for (var i = 2; i <= n; i++) {
+                if (isPrime[i]) {
+                    var idx = 2;
+                    while (true) {
+                        var x = i * idx;
+                        if (x > n) break;
+                        isPrime[x] = false;
+                        idx++;
+                    }
+                }
+            }
+
+            // 結果の表示
+            Console.WriteLine(isPrime[n] ? "YES" : "NO");
+        }
+    }
+}
