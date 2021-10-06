7212c2827e041ca63ff03d4307869e90f0f65a1c
itotto0205 <itotto0205@gmail.com>
2021-10-07 00:43:08 +0900

    フェルマーテスト追加

---
+++ fermattest001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fermattest001 {
+    /// <summary>
+    /// フェルマーテスト
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/prime_number_primer/prime_number_primer__fermat_test/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            const int RANDOM_NUM = 2; // 今回は2固定
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var fermat = 1;
+            for (var i = 1; i < n; i++) {
+                fermat *= RANDOM_NUM;
+                fermat %= n;
+            }
+
+            // 結果の表示
+            Console.WriteLine(fermat == 1 ? "YES" : "NO");
+        }
+    }
+}
