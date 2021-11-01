ef5f6948ae6a4c4ee2e143ead811f35294566a2b
itotto0205 <itotto0205@gmail.com>
2021-11-01 23:42:12 +0900

    中国剰余定理 追加（速度に課題あり）

---
+++ chineseremaindertheorem001/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace chineseremaindertheorem001 {
+    /// <summary>
+    /// 中国剰余定理
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/prime_number_primer/prime_number_primer__chinese_remainder_theorem/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            var inputs = Console.ReadLine().Split(' ');
+            var m1 = Convert.ToInt64(inputs[0]);
+            var m2 = Convert.ToInt64(inputs[1]);
+            var b1 = Convert.ToInt64(inputs[2]);
+            var b2 = Convert.ToInt64(inputs[3]);
+
+            long z = -1;
+
+            for (long i = 0; i < m1 * m2; i++) {
+                if (i % m1 == b1 && i % m2 == b2) {
+                    z = i;
+                    break;
+                }
+            }
+
+            // 結果の表示
+            Console.WriteLine(z);
+        }
+    }
+}
