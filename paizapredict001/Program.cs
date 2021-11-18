a0d6acf64bfa8e1bab6f8f564f996a1b1e1892c0
itotto0205 <itotto0205@gmail.com>
2021-11-19 00:38:34 +0900

    Paiza 予想追加（未実装）

---
+++ paizapredict001/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace paizapredict001 {
+    /// <summary>
+    /// paiza 予想
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/prime_number_primer/prime_number_primer__paiza_conjecture/edit?language_uid=c-sharp</remarks>
+    class Program {
+        static void Main() {
+            const int MAXNUM = 100000000;
+
+            // 2以上10^8以下の素数を求める
+            // 素数
+            var primes = new Dictionary<int, bool> { { 2, true }, { 3, true }, };
+            for (var i = 5; i < MAXNUM; i += 2) {
+                if (IsPrime(i)) primes.Add(i, true);
+            }
+
+
+            var targetNumbers = new List<int>();
+
+            // 結果の表示
+            if (targetNumbers.Count == 0) {
+                targetNumbers.Sort((x, y) => x - y);
+                targetNumbers.ForEach(t => Console.WriteLine(t));
+            } else {
+                Console.WriteLine("paiza's conjecture is correct.");
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int n) {
+            var limit = (long)Math.Sqrt(n) + 1;
+            for (var i = 3; i < limit; i += 2) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
