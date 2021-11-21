6ad6df40581a44f5e34799ec00fd285c65efaa11
itotto0205 <itotto0205@gmail.com>
2021-11-21 23:51:39 +0900

    指定の要素のカウントを実装

--- ElementCount001/Program.cs
+++ ElementCount001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections.Generic;
+/// <summary>
+/// 指定の要素のカウント 
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__count?language_uid=c-sharp</remarks>
+var inputs = Console.ReadLine().Split(' ');
+var n = Convert.ToInt32(inputs[0]);
+var k = Convert.ToInt32(inputs[1]);
+
+var cnt = 0;
+for (var i = 0; i < n; i++) {
+    if (Convert.ToInt32(Console.ReadLine()) == k) cnt++;
+}
+
+// 結果の表示
+Console.WriteLine(cnt);
