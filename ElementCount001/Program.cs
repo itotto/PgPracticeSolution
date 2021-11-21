e39b5cab3d11f0628a1820aacaa3980c772f4ba6
itotto0205 <itotto0205@gmail.com>
2021-11-21 23:52:45 +0900

    null許容演算子を適用

--- ElementCount001/Program.cs
+++ ElementCount001/Program.cs
@@ -1,12 +1,10 @@
-﻿
-using System.Collections.Generic;
-/// <summary>
+﻿/// <summary>
 /// 指定の要素のカウント 
 /// </summary>
 /// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__count?language_uid=c-sharp</remarks>
-var inputs = Console.ReadLine().Split(' ');
-var n = Convert.ToInt32(inputs[0]);
-var k = Convert.ToInt32(inputs[1]);
+var inputs = Console.ReadLine()?.Split(' ');
+var n = Convert.ToInt32(inputs?[0]);
+var k = Convert.ToInt32(inputs?[1]);
 
 var cnt = 0;
 for (var i = 0; i < n; i++) {
