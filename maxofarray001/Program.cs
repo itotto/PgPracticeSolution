e4b79ada18968663b0b9e138e9a1a1345a274e43
itotto0205 <itotto0205@gmail.com>
2021-11-24 23:59:40 +0900

    配列の最大値 追加

---
+++ maxofarray001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Generic;
+var n = Convert.ToInt32(Console.ReadLine());
+
+var list = new List<int>();
+for (int i = 0; i < n; i++) {
+    list.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+list.Sort((x, y) => x - y);
+
+// 結果の表示
+Console.WriteLine(list[list.Count - 1]);
+
