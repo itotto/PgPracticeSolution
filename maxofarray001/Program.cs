4263beb7e973bd2e81b608032308b3ddcb1a3a35
itotto0205 <itotto0205@gmail.com>
2021-11-25 00:01:18 +0900

    コメント追加

--- maxofarray001/Program.cs
+++ maxofarray001/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/// <summary>
+/// 配列の最大値
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__max/edit?language_uid=c-sharp</remarks>
 
-using System.Collections.Generic;
 var n = Convert.ToInt32(Console.ReadLine());
 
-var list = new List<int>();
+var list = new List<int>(n);
 for (int i = 0; i < n; i++) {
     list.Add(Convert.ToInt32(Console.ReadLine()));
 }
