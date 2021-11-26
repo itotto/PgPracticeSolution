e515a16ceb7e04f8f8407a7bbe4ffb3c7e8b2d13
itotto0205 <itotto0205@gmail.com>
2021-11-27 02:52:03 +0900

    配列の最小値追加

---
+++ minimumofarray001/Program.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 配列の最小値
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__min?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+
+var list = new List<int>(n);
+for (int i = 0; i < n; i++) {
+    list.Add(Convert.ToInt32(Console.ReadLine()));
+}
+
+list.Sort((x, y) => x - y);
+
+// 結果の表示
+Console.WriteLine(list[0]);
