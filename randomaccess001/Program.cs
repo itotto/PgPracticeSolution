12ca5f11172f22fa085859558e826aa936b7a385
itotto0205 <itotto0205@gmail.com>
2021-12-01 01:27:41 +0900

    ランダムアクセス 追加 複数回のランダムアクセス 追加

---
+++ randomaccess001/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// ランダムアクセス
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/data_structure/data_structure__array_step1?language_uid=c-sharp</remarks>
+var inputs = Console.ReadLine()?.Split(' ');
+_ = Convert.ToInt32(inputs?[0]);
+var m = Convert.ToInt32(inputs?[1]);
+
+// 結果の表示
+Console.WriteLine(Console.ReadLine()?.Split(' ')[m - 1]);
+
---
+++ randomaccess002/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 複数回のランダムアクセス
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/data_structure/data_structure__array_step2/edit?language_uid=c-sharp</remarks>
+
+_ = Convert.ToInt32(Console.ReadLine());
+var a = Console.ReadLine()?.Split(' ');
+
+_ = Convert.ToInt32(Console.ReadLine());
+foreach (var b in Console.ReadLine()?.Split(' ')) {
+    Console.WriteLine(a[Convert.ToInt32(b) - 1]);
+}
+
