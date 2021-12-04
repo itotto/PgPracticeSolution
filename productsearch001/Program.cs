503f08cc3a3c0c2619bbdf83b22a3d671d57d626
itotto0205 <itotto0205@gmail.com>
2021-12-05 01:14:54 +0900

    商品の検索 追加

---
+++ productsearch001/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 商品の検索
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/data_structure/data_structure__dict_boss/edit?language_uid=c-sharp</remarks>
+
+var inputs = Console.ReadLine()?.Split(' ');
+var n = Convert.ToInt32(inputs?[0]);
+var q = Convert.ToInt32(inputs?[1]);
+
+var products = new Dictionary<string, int>();
+for (var i = 1; i <= n; i++) {
+    var p = Console.ReadLine();
+    if (!products.ContainsKey(p)) products.Add(p, i);
+}
+
+// 結果を表示
+for (var i = 1; i <= q; i++) {
+    var s = Console.ReadLine();
+    Console.WriteLine(products.ContainsKey(s) ? products[s] : -1);
+}
+
