da5a618af23f35e42fed93dcedb5fa3f609858c3
itotto0205 <itotto0205@gmail.com>
2021-12-05 01:47:54 +0900

    集合の探索 追加

---
+++ searchingforaset001/Program.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// 集合の探索
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/data_structure/data_structure__set_step1/edit?language_uid=c-sharp</remarks>
+var data = Console.ReadLine()?.Split(' ');
+if (data == null) return;
+
+var line = Console.ReadLine();
+if (line == null) return;
+
+var r = false;
+foreach (var d in line.Split(' ')) {
+    if (d == data[1]) {
+        r = true;
+        break;
+    }
+}
+
+// 結果の表示
+Console.WriteLine(r ? "Yes" : "No");
