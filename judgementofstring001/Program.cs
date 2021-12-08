342713efbaef28229366106f03b9ba4f5daa4e4a
itotto0205 <itotto0205@gmail.com>
2021-12-09 02:18:11 +0900

    文字列の条件判定 追加

---
+++ judgementofstring001/Program.cs
@@ -0,0 +1,5 @@
+﻿/// <summary>
+/// 文字列の条件判定
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step4/edit?language_uid=c-sharp</remarks>
+Console.WriteLine(Console.ReadLine() == "paiza" ? "YES" : "NO");
