28ffe757744c7663bc6ec82263a8e9bf7811f92c
itotto0205 <itotto0205@gmail.com>
2021-12-09 02:15:55 +0900

    i 文字目の出力 追加

---
+++ i-thinput001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// i 文字目の出力
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step3/edit?language_uid=c-sharp</remarks>
+
+var s = Console.ReadLine();
+var i = Convert.ToInt32(Console.ReadLine()) - 1;
+
+// 結果の表示
+Console.WriteLine(s[i]);
