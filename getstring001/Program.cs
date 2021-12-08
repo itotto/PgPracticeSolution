88f05368ea202b93d73c4b2a71a7e4101fbac1c1
itotto0205 <itotto0205@gmail.com>
2021-12-09 02:13:33 +0900

    文字列の受け取り 追加

---
+++ getstring001/Program.cs
@@ -0,0 +1,5 @@
+﻿/// <summary>
+/// 文字列の受け取り
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step2/edit?language_uid=c-sharp</remarks>
+Console.WriteLine(Console.ReadLine());
