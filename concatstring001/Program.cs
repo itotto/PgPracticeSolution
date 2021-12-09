96fbe44b10f5d384b57b683f48ec202a9e3b2f4a
itotto0205 <itotto0205@gmail.com>
2021-12-10 00:31:52 +0900

    文字列の連結 追加

---
+++ concatstring001/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 文字列の連結
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step7/edit?language_uid=c-sharp</remarks>
+var n = Convert.ToInt32(Console.ReadLine());
+var list = new List<string>(n);
+
+for (int i = 0; i < n; i++) {
+    var line = Console.ReadLine();
+    if (line == null) continue;
+    list.Add(line);
+}
+
+list.ForEach(l => Console.Write(l));
+Console.WriteLine();
