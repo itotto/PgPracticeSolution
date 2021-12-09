b92fa0b2d4ea2ab2cf34f464f837f0b51c074010
itotto0205 <itotto0205@gmail.com>
2021-12-09 23:05:50 +0900

    文字の検索 追加

---
+++ searchstring001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 文字の検索
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step6/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+var c = Console.ReadLine();
+if (s == null || c == null) return;
+
+// 結果の表示
+Console.WriteLine(s.IndexOf(c[0]) + 1);
