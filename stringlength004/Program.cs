f52916a80b0f79d0de9e9d2c7a27683a6cf6e975
itotto0205 <itotto0205@gmail.com>
2021-12-09 23:00:14 +0900

    文字列の文字数 追加

---
+++ stringlength004/Program.cs
@@ -0,0 +1,5 @@
+﻿/// <summary>
+/// 文字列の文字数
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/basic_step5/edit?language_uid=c-sharp</remarks>
+Console.WriteLine(Console.ReadLine()?.Length);
