6385d6d1069a2e38876b4d91df7dfe38b0dfe589
itotto0205 <itotto0205@gmail.com>
2021-12-11 00:57:55 +0900

    文字列の挿入 追加

---
+++ insertstring001/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// 文字列の挿入
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step2/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+var t = Console.ReadLine();
+var n = Convert.ToInt32(Console.ReadLine());
+
+if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return;
+
+// 結果の表示
+Console.WriteLine(s.Insert(n - 1, t));
