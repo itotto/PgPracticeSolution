ea2f949ab18e2b96d6ce49d198b58fba5b188544
itotto0205 <itotto0205@gmail.com>
2021-12-12 01:26:29 +0900

    文字列の書き換え 追加

---
+++ changestring001/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 文字列の書き換え
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step3/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+var inputs = Console.ReadLine()?.Split(' ');
+if (inputs == null) return;
+var i = Convert.ToInt32(inputs[0]) - 1;
+var c = inputs[1][0];
+
+    // 結果の表示
+for (var j = 0; j < s?.Length; j++) {
+    Console.Write($"{(i == j ? c : s[j])}");
+}
+Console.WriteLine();
