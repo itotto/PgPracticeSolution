a0382b9a2598892cf91a75aa2adafacc2bc7cba5
itotto0205 <itotto0205@gmail.com>
2021-12-14 00:05:39 +0900

    数値から文字列への変換 追加

---
+++ int2string001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 数値から文字列への変換
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step5/edit?language_uid=c-sharp</remarks>
+var x = Convert.ToInt32(Console.ReadLine());
+var y = Convert.ToInt32(Console.ReadLine());
+var n = Convert.ToInt32(Console.ReadLine());
+
+// 結果の表示
+Console.WriteLine((x + y).ToString()[n - 1]);
