66bbe15272bfdf9becc5ddcaf597f3fc125d4a23
itotto0205 <itotto0205@gmail.com>
2021-12-13 23:25:37 +0900

    文字列から数値への変換 追加

---
+++ string2int001/Program.cs
@@ -0,0 +1,6 @@
+﻿/// <summary>
+/// 文字列から数値への変換
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step4/edit?language_uid=c-sharp</remarks>
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) - 813);
+
