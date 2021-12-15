d3a649caa22576dc1ce840340c32a6c0b393e155
itotto0205 <itotto0205@gmail.com>
2021-12-16 00:26:49 +0900

    小文字から大文字への変換 追加

---
+++ lowercase2uppercase001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 小文字から大文字への変換
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step7/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+foreach (var c in s) {
+    Console.Write(c > 0x5a ? (char)((int)c - 0x20) : c);
+}
+Console.WriteLine();
