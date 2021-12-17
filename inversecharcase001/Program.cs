854415ec823101163a12523779c360cdf74bcc72
itotto0205 <itotto0205@gmail.com>
2021-12-17 11:04:35 +0900

    大文字小文字の反転 追加

---
+++ inversecharcase001/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// 大文字小文字の反転
+/// </summary>
+/// <remarksa>https://paiza.jp/works/mondai/string_primer/normal_step8/edit?language_uid=c-sharp</remarksa>
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+foreach (var c in s) {
+    var ic = (int)c;
+    Console.Write((0x41 <= ic && ic <= 0x5A) ? (char)(c + 0x20) : (char)(c - 0x20));
+}
+Console.WriteLine();
+
