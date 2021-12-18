b43db727193bdf69e4654d6388c59c9ff3b5912c
itotto0205 <itotto0205@gmail.com>
2021-12-18 23:52:11 +0900

    文字列の検索 追加

---
+++ searchofstring001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 文字列の検索
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step9/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+var t = Console.ReadLine();
+if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return;
+
+// 結果の表示
+Console.WriteLine(s.IndexOf(t) >= 0 ? "YES" : "NO");
