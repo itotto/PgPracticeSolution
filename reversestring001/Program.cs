34d88ea75a07cbfd0a39d9705148f6f6bd769d20
itotto0205 <itotto0205@gmail.com>
2021-12-20 01:42:29 +0900

    文字列の反転 追加

---
+++ reversestring001/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// 文字列の反転
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step10/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+for(var i = s.Length - 1; i >= 0; i--) {
+    Console.Write(s[i]);
+}
+Console.WriteLine();
