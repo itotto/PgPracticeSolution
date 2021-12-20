6393c64177e9115679661a294f22c4eb336bfac3
itotto0205 <itotto0205@gmail.com>
2021-12-21 00:18:16 +0900

    回文判定 追加

---
+++ palindrome001/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 回文判定
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/normal_step11/edit?language_uid=c-sharp</remarks>
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+var pos = s.Length - 1;
+var isPalindrome = true;
+
+for (var i = 0; i < s.Length; i++) {
+    if (s[i] != s[pos - i]) {
+        isPalindrome = false;
+        break;
+    }
+}
+
+// 結果の表示
+Console.WriteLine(isPalindrome ? "YES" : "NO");
+
