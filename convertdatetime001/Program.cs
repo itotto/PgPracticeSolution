82e1ae1ac299eb77d5be582936ab234abe0de9a9
itotto0205 <itotto0205@gmail.com>
2021-12-22 01:05:16 +0900

    日時データの変換その１ 追加

---
+++ convertdatetime001/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 日時データの変換その１
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step2/edit?language_uid=c-sharp</remarks>
+
+var dtstr = Console.ReadLine()?.Split('/');
+if (dtstr == null) return;
+
+Console.WriteLine(dtstr[0]);
+Console.WriteLine(dtstr[1]);
+Console.WriteLine(dtstr[2]);
+Console.WriteLine($"{dtstr[3].Split(':')[0]:00}");
+Console.WriteLine($"{dtstr[3].Split(':')[1]:00}");
