ae0896fa564a0bf83b8a7280219ef6211a2b8220
itotto0205 <itotto0205@gmail.com>
2021-12-22 01:07:47 +0900

    エラーチェックを強化 リファクタリング

--- convertdatetime001/Program.cs
+++ convertdatetime001/Program.cs
@@ -4,10 +4,13 @@
 /// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step2/edit?language_uid=c-sharp</remarks>
 
 var dtstr = Console.ReadLine()?.Split('/');
-if (dtstr == null) return;
+if (dtstr == null || dtstr.Length != 4) return;
+
+var tmstrs = dtstr[3].Split(':');
+if (tmstrs == null || tmstrs.Length != 2) return;
 
 Console.WriteLine(dtstr[0]);
-Console.WriteLine(dtstr[1]);
-Console.WriteLine(dtstr[2]);
-Console.WriteLine($"{dtstr[3].Split(':')[0]:00}");
-Console.WriteLine($"{dtstr[3].Split(':')[1]:00}");
+Console.WriteLine($"{dtstr[1]:00}");
+Console.WriteLine($"{dtstr[2]:00}");
+Console.WriteLine($"{tmstrs[0]:00}");
+Console.WriteLine($"{tmstrs[1]:00}");
