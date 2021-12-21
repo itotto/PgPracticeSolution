31ebf88debe8351a68b026ddb9dce970ec15334e
itotto0205 <itotto0205@gmail.com>
2021-12-22 00:04:56 +0900

    文字列の分割 追加

---
+++ splitstring002/Program.cs
@@ -0,0 +1,5 @@
+﻿/// <summary>
+/// 文字列の分割
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step1/edit?language_uid=c-sharp</remarks>
+Console.ReadLine()?.Split(',').ToList().ForEach(s => Console.WriteLine(s));
