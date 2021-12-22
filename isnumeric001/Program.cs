be945d19e05f4b12a00dddf256d5df3cca1fa8e4
itotto0205 <itotto0205@gmail.com>
2021-12-23 02:11:55 +0900

    数値判定 追加

---
+++ isnumeric001/Program.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// 数値判定
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step4/edit?language_uid=c-sharp</remarks>
+
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+var minValue = (int)'0';
+var maxValue = (int)'9';
+
+var isnumeric = true;
+
+foreach (var c in s) {
+    if (c < minValue || maxValue < c) {
+        isnumeric = false;
+        break;
+    }
+}
+
+// 結果の表示
+Console.WriteLine(isnumeric ? "YES" : "NO");
