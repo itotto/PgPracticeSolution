e1860803aa2f1b5b0a997b938ab0ea921886c933
itotto0205 <itotto0205@gmail.com>
2021-12-24 00:13:44 +0900

    重複の削除 追加

---
+++ removeduplicate001/Program.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 重複の削除
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step5/edit?language_uid=c-sharp</remarks>
+
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+var nums = new Dictionary<char, bool>();
+
+foreach (var c in s) {
+    if (!nums.ContainsKey(c)) {
+        nums.Add(c, true);
+        Console.Write(c);
+    }
+}
