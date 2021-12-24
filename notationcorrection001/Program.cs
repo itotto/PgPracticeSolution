480c5066810a6d016ddec38aee382e810f4ba9dc
itotto0205 <itotto0205@gmail.com>
2021-12-25 00:44:09 +0900

    表記の訂正 追加

---
+++ notationcorrection001/Program.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 表記の訂正
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step7/edit?language_uid=c-sharp</remarks>
+
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+
+// check1
+
+
+
+// check2
+
+
+// check3
+
+
+
+
