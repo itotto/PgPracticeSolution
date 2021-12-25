389c2819f05fd9a24c524483286465fb73bf1c32
itotto0205 <itotto0205@gmail.com>
2021-12-26 02:42:21 +0900

    数式の計算（ 1 桁） 追加

---
+++ calculation001/Program.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// 数式の計算（1桁）
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/string_primer/advance_step8/edit?language_uid=c-sharp</remarks>
+
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+var answer = Convert.ToInt32(s[0] - '0');
+for (int i = 1; i < s.Length - 1; i += 2) {
+    if (s[i] == '+') answer += Convert.ToInt32(s[i + 1] - '0');
+    else answer -= Convert.ToInt32(s[i + 1] - '0');
+}
+
+// 結果の表示
+Console.WriteLine(answer);
