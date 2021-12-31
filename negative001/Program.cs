dd32280f6c9121134d724d46ebcddbe746bffe31
itotto0205 <itotto0205@gmail.com>
2022-01-01 02:23:36 +0900

    否定( NOT )の基本 追加

---
+++ negative001/Program.cs
@@ -0,0 +1,6 @@
+﻿/// <summary>
+/// 否定( NOT )の基本
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step3/edit?language_uid=c-sharp</remarks>
+
+Console.WriteLine(Console.ReadLine() == "1" ? "0" : "1");
