0338830d13559ebfaa670b86b9afa6573e3f5a82
itotto0205 <itotto0205@gmail.com>
2022-01-01 01:49:42 +0900

    論理積( AND )の基本追加

---
+++ logicalproduct001/Program.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// 論理積( AND )の基本
+/// </summary>
+/// <remarks></remarks>
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+
+// 結果の表示
+Console.WriteLine(v[0] == "1" && v[1] == "1" ? "1" : "0");
