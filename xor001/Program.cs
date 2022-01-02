1160e6999bcd1364ae8aafe3f75607ebae5389e8
itotto0205 <itotto0205@gmail.com>
2022-01-02 23:53:04 +0900

    排他的論理和( XOR )の基本 追加

---
+++ xor001/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using System.Collections.Generic;
+/// <summary>
+/// 排他的論理和( XOR )の基本
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step4/edit?language_uid=c-sharp</remarks>
+
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+
+// 結果の表示
+Console.WriteLine(v[0] == "1" ^ v[1] == "1" ? "1" : "0");
