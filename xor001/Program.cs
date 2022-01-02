51f19e7387f2ab4350a07f58c2ec5aff7f67fd10
itotto0205 <itotto0205@gmail.com>
2022-01-02 23:55:31 +0900

    不要なusingを削除

--- xor001/Program.cs
+++ xor001/Program.cs
@@ -1,8 +1,4 @@
-﻿
-
-
-using System.Collections.Generic;
-/// <summary>
+﻿/// <summary>
 /// 排他的論理和( XOR )の基本
 /// </summary>
 /// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step4/edit?language_uid=c-sharp</remarks>
