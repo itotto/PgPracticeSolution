6df8cd05fb83d5afac15ea55011b6c2fb4b5eadc
itotto0205 <itotto0205@gmail.com>
2022-01-04 00:50:24 +0900

    NOR 演算の基本 追加

---
+++ nor001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// NOR 演算の基本
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step6/edit?language_uid=c-sharp</remarks>
+
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+
+// 結果の表示
+Console.WriteLine(!(v[0] == "1" || v[1] == "1") ? "1" : "0");
