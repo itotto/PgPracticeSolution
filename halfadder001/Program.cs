150f97873d9ba4f9641f9cb2d936c3acc0a55075
itotto0205 <itotto0205@gmail.com>
2022-01-07 00:35:42 +0900

    半加算器 追加

---
+++ halfadder001/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 半加算器
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step8/edit?language_uid=c-sharp</remarks>
+
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+
+var a = v[0] == "1";
+var b = v[1] == "1";
+
+// 結果の表示
+Console.WriteLine($"{(a && b ? "1" : "0")} {(a != b ? "1" : "0")}");
