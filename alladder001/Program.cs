616e64edff5a03e1436401466bb589171ca7db84
itotto0205 <itotto0205@gmail.com>
2022-01-09 02:25:42 +0900

    全加算器追加

---
+++ alladder001/Program.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// 全加算器
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_step9/edit?language_uid=c-sharp</remarks>
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+
+var a = v[0] == "1";
+var b = v[1] == "1";
+var c1 = v[2] == "1";
+
+
+// 結果の表示
+Console.WriteLine($"{((a && b) || (a && c1) || (b && c1) ? "1" : "0")} {(a ^ b ^ c1 ? "1" : "0")}");
