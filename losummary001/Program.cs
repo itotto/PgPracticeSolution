67a03306793e92471ac02bb5cd4882f762065172
itotto0205 <itotto0205@gmail.com>
2022-01-10 01:46:56 +0900

    論理演算を用いた計算のまとめ追加

---
+++ losummary001/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 論理演算を用いた計算のまとめ
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/logical_operation/logical_operation__basic_boss/edit?language_uid=c-sharp</remarks>
+var v = Console.ReadLine()?.Split(' ');
+if (v == null) return;
+var a = v[0] == "1";
+var b = v[1] == "1";
+var c = v[2] == "1";
+var d = v[3] == "1";
+
+// 結果の表示
+Console.WriteLine(((a || b) && c) ^ d ? 1 : 0);
