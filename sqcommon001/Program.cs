67b90fcc1856e6b2a085540c6c96a5793c10807e
itotto0205 <itotto0205@gmail.com>
2022-01-15 01:41:49 +0900

    スタック・キュー実装編( 共通問題 ) step 1追加 スタック・キュー実装編( 共通問題 ) step 2 追加

---
+++ sqcommon001/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// スタック・キュー実装編( 共通問題 ) step 1
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/stack_queue/stack_queue__common_step1/edit?language_uid=c-sharp</remarks>
+var n = Convert.ToInt32(Console.ReadLine());
+
+var data = new int[n];
+
+for(var i = 0; i < n; i++) {
+    data[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// 結果の表示
+Console.WriteLine(n);
+
+for (var i = 0; i < n; i++) {
+    Console.WriteLine(data[i]);
+}
+
---
+++ sqcommon002/Program.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// スタック・キュー実装編( 共通問題 ) step 2
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/stack_queue/stack_queue__common_step2/edit?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+
+var data = new int[n];
+var cnt = 0;
+for(int i = 0; i < n; i++) {
+    var v = Console.ReadLine()?.Split(' ');
+    if (v == null || v.Length == 1) continue;
+    data[cnt++] = Convert.ToInt32(v[1]); 
+}
+// 配列の大きさを変更する
+Array.Resize(ref data, cnt);
+
+
+Console.WriteLine(data.Length);
+for (var i = 0; i < data.Length; i++) {
+    Console.WriteLine(data[i]);
+}
