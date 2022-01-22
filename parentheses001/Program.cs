88f093a92ed1f2f0aa4c37d1d8fa044aee363b64
itotto0205 <itotto0205@gmail.com>
2022-01-23 01:18:46 +0900

    括弧列 追加

---
+++ parentheses001/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 括弧列
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/stack_queue/stack_queue__practice_step4/edit?language_uid=c-sharp</remarks>
+
+_ = Convert.ToInt32(Console.ReadLine());
+
+var str = Console.ReadLine();
+if (string.IsNullOrEmpty(str)) return;
+
+var cnt = 0;
+foreach (var c in str) {
+    if (c == '(') cnt++;
+    else if (c == ')') cnt--;
+    if (cnt < 0) break;
+}
+
+// 結果の表示
+Console.WriteLine(cnt == 0 ? "Yes" : "No");
