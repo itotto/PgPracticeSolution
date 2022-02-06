bcef241a82a81706a03275a98b802c5e577a4397
itotto0205 <itotto0205@gmail.com>
2022-02-07 02:21:50 +0900

    隣接リストの出力・無向グラフ 追加（未了） 指定要素の先頭位置 追加

---
+++ adjacencylist_undirectedgraph001/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+using System.Collections.Generic;
+/// <summary>
+/// 隣接リストの出力・無向グラフ
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__adjacency_list_step1/edit?language_uid=c-sharp</remarks>
+
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var countOfVertexes = Convert.ToInt32(vals[0]);
+var counteOfSides = Convert.ToInt32(vals[1]);
---
+++ firstposition001/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 指定要素の先頭位置
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__find/edit?language_uid=c-sharp</remarks>
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var n = Convert.ToInt32(vals[0]);
+var k = vals[1];
+
+var data = new string[n];
+
+for (var i = 0; i < n; i++) data[i] = Console.ReadLine();
+
+var pos = -1;
+
+for (var i = 0; i < n; i++) {
+    if (data[i] == k) {
+        pos = i + 1;
+        break;
+    }
+}
+
+
+// 結果の表示
+Console.WriteLine(pos);
+
