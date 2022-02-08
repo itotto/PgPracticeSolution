d5cd77f7e71d4d02588a44cddd6d58fd96983d03
itotto0205 <itotto0205@gmail.com>
2022-02-08 23:56:54 +0900

    要素の種類数 追加

---
+++ kindofelements001/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Collections.Generic;
+/// <summary>
+/// 要素の種類数
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__kind/edit?language_uid=c-sharp</remarks>
+var n = Convert.ToInt32(Console.ReadLine());
+
+var cnt = 0;
+var data = new string[n];
+for (var i = 0; i < n; i++) {
+    var s = Console.ReadLine();
+    if (string.IsNullOrEmpty(s)) continue;
+    var notfound = true;
+    for (var j = 0; j < i; j++) {
+        if (data[j] == s) {
+            notfound = false;
+            break;
+        }
+    }
+    if (notfound) cnt++;
+    data[i] = s;
+}
+
+// 結果の表示
+Console.WriteLine(cnt);
+
