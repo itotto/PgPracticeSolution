13dede78fa6d04d82d3329c87df20075d7a8aadb
itotto0205 <itotto0205@gmail.com>
2022-02-09 01:29:36 +0900

    全ての要素に対する操作 追加

---
+++ operation2allelements001/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+/// <summary>
+/// 全ての要素に対する操作
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__map/edit?language_uid=c-sharp</remarks>
+
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var n = Convert.ToInt32(vals[0]);
+var k = Convert.ToInt32(vals[1]);
+
+var r = new StringBuilder();
+
+for (var i = 0; i < n; i++) {
+    r.AppendLine($"{(Convert.ToInt32(Console.ReadLine()) + k)}");
+}
+
+// 結果の表示
+Console.WriteLine(r.ToString());
