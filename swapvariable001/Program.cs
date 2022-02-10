2a946efd61e03021d4f2644a154665ea28b9656f
itotto0205 <itotto0205@gmail.com>
2022-02-11 01:41:02 +0900

    変数の入れ替え追加

---
+++ swapvariable001/Program.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// 変数の入れ替え
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__excange/edit?language_uid=c-sharp</remarks>
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+
+// 結果の表示
+Console.WriteLine($"{vals[1]} {vals[0]}");
