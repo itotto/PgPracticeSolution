b3b0cded462114ab39f19c82fadccf8da9a475a4
itotto0205 <itotto0205@gmail.com>
2022-02-16 00:52:37 +0900

    配列のサイズの変更 追加 重複要素の削除 追加

---
+++ changearraysize001/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 配列のサイズの変更
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__resize/edit?language_uid=c-sharp</remarks>
+const int DEFVAL = 0;
+
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var firstSize = Convert.ToInt32(vals[0]);
+var realSize = Convert.ToInt32(vals[1]);
+
+var data = new int[firstSize > realSize ? firstSize : realSize];
+for(var i = 0; i < data.Length; i++) data[i] = DEFVAL;
+for(var i = 0; i < firstSize; i++) {
+    data[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+// 結果の表示
+for (var i = 0; i < realSize; i++) Console.WriteLine(data[i]);
---
+++ removeduplicateelement001/Program.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// 重複要素の削除
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__unique/edit?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+var data = new string[n];
+for (int i = 0; i < n; i++) {
+    var t = Console.ReadLine();
+    if (string.IsNullOrEmpty(t)) continue;
+    data[i] = t;
+}
+
+var dataCopy = new string[n];
+var dataCnt = 0;
+
+for (int i = 0; i < n; i++) {
+    var bfound = false;
+    for (var j = 0; j < dataCnt; j++) {
+        if (data[i] == dataCopy[j]) {
+            bfound = true;
+            break;
+        }
+    }
+
+    if (!bfound) {
+        dataCopy[dataCnt++] = data[i];
+    }
+}
+
+// 結果を表示
+for (var i = 0; i < dataCnt; i++) Console.WriteLine(dataCopy[i]);
+
