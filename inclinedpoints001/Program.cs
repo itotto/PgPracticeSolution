335496c03570558c478c8ee1c8a5dadba3b65ee7
itotto0205 <itotto0205@gmail.com>
2022-02-18 00:21:46 +0900

    傾斜配点 追加 内定 追加

---
+++ inclinedpoints001/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// 傾斜配点
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__exam/edit?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+
+var weightsArray = Console.ReadLine()?.Split(' ');
+if (weightsArray == null) return;
+var weights = new int[weightsArray.Length];
+for (var i = 0; i < weights.Length; i++) {
+    weights[i] = Convert.ToInt32(weightsArray[i]);
+}
+
+var maxPoint = 0;
+
+for (var i = 0; i < n; i++) {
+    var vals = Console.ReadLine()?.Split(' ');
+    if (vals == null) return;
+    var points = 0;
+    for (var j = 0; j < weights.Length; j++) {
+        points += (Convert.ToInt32(vals[j]) * weights[j]);
+    }
+    if (maxPoint < points) maxPoint = points;
+}
+
+// 結果の表示
+Console.WriteLine(maxPoint);
---
+++ unofficialdicision001/Program.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 内定
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__offer/edit?language_uid=c-sharp</remarks>
+
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var n = Convert.ToInt32(vals[0]);
+var k = Convert.ToInt32(vals[1]);
+var m = Convert.ToInt32(vals[2]);
+
+var cnt = 0;
+for (var i = 0; i < n; i++) {
+    var point = Convert.ToInt32(Console.ReadLine());
+    if (point >= k) cnt++;
+}
+
+// 結果の表示
+Console.WriteLine(cnt - m > 0 ? cnt - m : 0);
