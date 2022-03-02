0fe73aecaf54f8bbc863354533e6e2989b52b5be
itotto0205 <itotto0205@gmail.com>
2022-03-03 01:05:46 +0900

    連想配列 追加

---
+++ associativearray001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+/// <summary>
+/// 連想配列
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/query_primer/query_primer__map_easy/edit?language_uid=c-sharp</remarks>
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var n = Convert.ToInt32(vals[0]);
+var k = Convert.ToInt32(vals[1]);
+
+var data = new Dictionary<int, string>();
+for (int i = 0; i < n; i++) {
+    var inputs = Console.ReadLine()?.Split(' ');
+    if (inputs == null) return;
+    data.Add(Convert.ToInt32(inputs[0]),inputs[1]);
+}
+
+var r = new StringBuilder();
+
+for (int i = 0; i < k; i++) {
+   r.AppendLine(data[Convert.ToInt32(Console.ReadLine())]);
+}
+
+// 結果の表示
+Console.WriteLine($"{r}");
