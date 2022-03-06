4e897445c9931e131e67024345532464f458d581
itotto0205 <itotto0205@gmail.com>
2022-03-07 01:22:49 +0900

    スポーツの人数 追加

---
+++ numberofmembers001/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// スポーツの人数
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_001/forest_contest_001__d_sports/edit?language_uid=c-sharp</remarks>
+var memberInfo = new Dictionary<string, int> {
+    {"baseball", 9 }, {"soccer", 11 }, {"rugby", 15 }, {"basketball", 5 }, {"volleyball", 6 }
+};
+
+var s = Console.ReadLine();
+if (string.IsNullOrEmpty(s)) return;
+
+Console.WriteLine(memberInfo[s]);
