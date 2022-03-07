1c9b6722097c8beabf811c7872e239cd39584460
itotto0205 <itotto0205@gmail.com>
2022-03-08 01:52:08 +0900

    バイトの休憩時間 追加

---
+++ resttime001/Program.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// バイトの休憩時間
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_001/forest_contest_001__d_break_time/edit?language_uid=c-sharp</remarks>
+
+var h = Convert.ToInt32(Console.ReadLine());
+
+// 結果の表示
+Console.WriteLine(h <= 6 ? "no break" : $"{(h > 8 ? 60 : 45)} min");
