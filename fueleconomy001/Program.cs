9bce05f0fa3a4f1771933b4ac567779342e184ec
itotto0205 <itotto0205@gmail.com>
2022-03-09 01:49:52 +0900

    自動車の燃費 追加 背番号順 追加

---
+++ fueleconomy001/Program.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// 自動車の燃費
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_001/forest_contest_001__d_car_fuel/edit?language_uid=c-sharp</remarks>
+
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var v = Convert.ToInt32(vals[0]);
+var d = Convert.ToInt32(vals[1]);
+
+// 結果の表示
+Console.WriteLine($"{d / v}");
+
---
+++ uniformnumberorder001/Program.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// 背番号順
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_001/forest_contest_001__c_player_number/edit?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+
+var members = new List<PlayerInfo>(n);
+
+for (var i = 0; i < n; i++) {
+    var vals = Console.ReadLine()?.Split(' ');
+    if (vals == null) return;
+    members.Add(new PlayerInfo {
+        PlayerNumber = Convert.ToInt32(vals[0]),
+        PlayerName = vals[1],
+    });
+}
+
+members.Sort((x, y) => x.PlayerNumber - y.PlayerNumber);
+members.ForEach(m => Console.WriteLine($"{m.PlayerNumber} {m.PlayerName}"));
+
+
+/// <summary>
+/// 構造体クラス
+/// </summary>
+internal class PlayerInfo {
+    /// <summary>
+    /// プレイヤーの名前
+    /// </summary>
+    public string PlayerName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// プレイヤー番号
+    /// </summary>
+    public int PlayerNumber { get; set; } = 0;
+}
