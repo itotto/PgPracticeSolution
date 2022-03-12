a3be50c9d485ac1c119e2028cfd7346ca7841b53
itotto0205 <itotto0205@gmail.com>
2022-03-13 03:13:41 +0900

    速度改善(未了)

--- idlegroup001/Program.cs
+++ idlegroup001/Program.cs
@@ -1,8 +1,10 @@
-﻿/// <summary>
+﻿
+using System.Collections.Generic;
+using System.Text;
+/// <summary>
 /// アイドルグループ
 /// </summary>
 /// <remarks>https://paiza.jp/works/mondai/query_primer/query_primer__idle_group/edit?language_uid=c-sharp</remarks>
-
 var vals = Console.ReadLine()?.Split(' ');
 if (vals == null) return;
 var n = Convert.ToInt32(vals[0]);
@@ -15,14 +17,28 @@ for (var i = 0; i < n; i++) {
     members.Add(m);
 }
 
+
+var deletedData = new Dictionary<string, bool>(n);
+var r = new StringBuilder();
 for (var i = 0; i < k; i++) {
     var query = Console.ReadLine()?.Split(' ');
     if (query == null || query.Length == 0) continue;
 
     switch (query.Length) {
         case 1:
-            members.Sort((x,y) => x.CompareTo(y));
-            for (var j = 0; j < members.Count; j++) Console.WriteLine(members[j]);
+            if (deletedData.Count > 0) {
+                for (var j = members.Count - 1; j >= 0; j--) {
+                    if (deletedData.Count == 0) break;
+                    var m = members[j];
+                    if (deletedData.ContainsKey(m)) {
+                        members.RemoveAt(j);
+                        deletedData.Remove(m);
+                    }
+                }
+            }
+
+            members.Sort((x, y) => x.CompareTo(y));
+            for (var j = 0; j < members.Count; j++) r.AppendLine(members[j]);
             break;
         case 2:
             var name = query[1];
@@ -30,7 +46,8 @@ for (var i = 0; i < k; i++) {
                 members.Add(name);
             } else if (query[0] == "leave") {
                 //members.RemoveAt(indexes[name]);
-                members.Remove(name);
+                //members.Remove(name);
+                deletedData.Add(name, true);
             }
             break;
 
@@ -38,4 +55,5 @@ for (var i = 0; i < k; i++) {
             break;
     }
 }
+Console.Write(r.ToString());
 
