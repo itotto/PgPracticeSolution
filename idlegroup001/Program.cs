b171a4aae6d9c1f9c7ab7a6ea60989f42e2af28a
itotto0205 <itotto0205@gmail.com>
2022-03-14 02:33:19 +0900

    switchを使わないよう修正(でもまだ遅い)

--- idlegroup001/Program.cs
+++ idlegroup001/Program.cs
@@ -12,47 +12,32 @@ var k = Convert.ToInt32(vals[1]);
 
 var members = new List<string>(100000);
 for (var i = 0; i < n; i++) {
-    var m = Console.ReadLine();
-    if (string.IsNullOrEmpty(m)) continue;
-    members.Add(m);
+    members.Add(Console.ReadLine());
 }
-
 
 var deletedData = new Dictionary<string, bool>(n);
 var r = new StringBuilder();
 for (var i = 0; i < k; i++) {
     var query = Console.ReadLine()?.Split(' ');
-    if (query == null || query.Length == 0) continue;
 
-    switch (query.Length) {
-        case 1:
-            if (deletedData.Count > 0) {
-                for (var j = members.Count - 1; j >= 0; j--) {
-                    if (deletedData.Count == 0) break;
-                    var m = members[j];
-                    if (deletedData.ContainsKey(m)) {
-                        members.RemoveAt(j);
-                        deletedData.Remove(m);
-                    }
+    if (query[0] == "handshake") {
+        if (deletedData.Count > 0) {
+            for (var j = members.Count - 1; j >= 0; j--) {
+                if (deletedData.Count == 0) break;
+                var m = members[j];
+                if (deletedData.ContainsKey(m)) {
+                    members.RemoveAt(j);
+                    deletedData.Remove(m);
                 }
             }
+        }
 
-            members.Sort((x, y) => x.CompareTo(y));
-            for (var j = 0; j < members.Count; j++) r.AppendLine(members[j]);
-            break;
-        case 2:
-            var name = query[1];
-            if (query[0] == "join") {
-                members.Add(name);
-            } else if (query[0] == "leave") {
-                //members.RemoveAt(indexes[name]);
-                //members.Remove(name);
-                deletedData.Add(name, true);
-            }
-            break;
-
-        default:
-            break;
+        members.Sort((x, y) => x.CompareTo(y));
+        for (var j = 0; j < members.Count; j++) r.AppendLine(members[j]);
+    } else if (query[0] == "join") {
+        members.Add(query[1]);
+    } else if (query[0] == "leave") {
+        deletedData.Add(query[1], true);
     }
 }
 Console.Write(r.ToString());
