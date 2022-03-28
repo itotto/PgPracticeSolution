eb7943baf9aff44b04acb449a7fd775163dc7923
itotto0205 <itotto0205@gmail.com>
2022-03-29 02:06:12 +0900

    競輪のユニフォーム 追加

---
+++ uniform001/Program.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// 競輪のユニフォーム
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_002/forest_contest_002__d_keirin/edit?language_uid=c-sharp</remarks>
+
+var n = Convert.ToInt32(Console.ReadLine());
+var c = string.Empty;
+switch (n) {
+    case 1:
+        c = "white";
+        break;
+    case 2:
+        c = "black";
+        break;
+    case 3:
+        c = "red";
+        break;
+    case 4:
+        c = "blue";
+        break;
+    case 5:
+        c = "yellow";
+        break;
+    case 6:
+        c = "green";
+        break;
+    case 7:
+        c = "orange";
+        break;
+    case 8:
+        c = "pink";
+        break;
+    case 9:
+        c = "purple";
+        break;
+}
+
+// 結果の表示
+Console.WriteLine(c);
