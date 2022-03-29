c660d5da60ebf3e31aadc0a5b2493df99d570fe0
itotto0205 <itotto0205@gmail.com>
2022-03-30 02:43:32 +0900

    エレベーターの行き先 追加

---
+++ destinationofelevator001/Program.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// エレベーターの行き先
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_002/forest_contest_002__d_elevator/edit?language_uid=c-sharp</remarks>
+var inputs = Console.ReadLine()?.Split(' ');
+if (inputs == null) return;
+var currentFloor = Convert.ToInt32(inputs[0][0]);
+var nextFloor = Convert.ToInt32(inputs[1][0]);
+
+// 結果の表示
+Console.WriteLine(currentFloor > nextFloor ? "down" : "up");
+
