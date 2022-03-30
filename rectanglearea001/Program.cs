be7f67ac5ffe8cda120375d181655e6b5f88f654
itotto0205 <itotto0205@gmail.com>
2022-03-31 02:49:28 +0900

    長方形の面積 追加

---
+++ rectanglearea001/Program.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// 長方形の面積
+/// </summary>
+/// <remarks>https://paiza.jp/works/mondai/forest_contest_002/forest_contest_002__d_rectangular/edit?language_uid=c-sharp</remarks>
+var vals = Console.ReadLine()?.Split(' ');
+if (vals == null) return;
+var area = Convert.ToInt32(vals[0]) * Convert.ToInt32(vals[1]);
+
+// 結果の表示
+Console.WriteLine(area == 0 ? "invalid" : area.ToString());
